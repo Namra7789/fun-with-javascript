899a2a1b742fe1014d83b662e7d54dd90b290af1
Namra7789 <130475187+Namra7789@users.noreply.github.com>
2023-10-09 07:55:25 +0000

    container config done

---
+++ javaSbasics/02_conversionOperation.js
@@ -0,0 +1,20 @@
+let score ="33"
+
+console.log(typeof (score));
+
+let valInNumber = Number(score)  //'N' in Number should be in cap. Simillarly, S of String or B of Boolean
+console.log(typeof (valInNumber));
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false;
+//""=> false
+// "hello"=> true
+
+let numVal = 23
+let stringVal = String(numVal)
+console.log(stringVal);
+console.log(typeof(stringVal));
